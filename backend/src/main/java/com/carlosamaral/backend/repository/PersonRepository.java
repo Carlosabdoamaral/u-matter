21923d7f240e6348f4fa69034238df87ef8604c2
Carlos Amaral <carlos.a.amaral@sesisc.org.br>
2022-06-20 12:36:04 -0300

    Adding code

--- backend/src/main/java/com/carlosamaral/backend/repository/PersonRepository.java
+++ backend/src/main/java/com/carlosamaral/backend/repository/PersonRepository.java
@@ -1,8 +1,12 @@
 package com.carlosamaral.backend.repository;
 import com.carlosamaral.backend.model.PersonModel;
+import org.hibernate.mapping.Any;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
+import javax.transaction.Transactional;
 import java.util.Date;
 
 public interface PersonRepository extends JpaRepository<PersonModel, Long> {
@@ -14,14 +18,20 @@ public interface PersonRepository extends JpaRepository<PersonModel, Long> {
             @Param("startDate") Date startDate
     );
 
+    @Transactional
+    @Modifying
     @Query(value = "UPDATE person SET deleted_account = true WHERE uuid = :person_uuid", nativeQuery = true)
     void softDeleteUser(
             @Param("person_uuid") String person_uuid
     );
 
-    @Query(value = "UPDATE person SET blocked_account = true, last_block_time = :block_time WHERE person_id = :person_uuid", nativeQuery = true)
-    void blockPerson(
-            @Param("person_uuid") String person_uuid,
-            @Param("block_time") Double block_time
-    );
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE person SET last_block_time = last_block_time + 2, blocked_account = true WHERE person_id = :person_id", nativeQuery = true)
+    void increaseBlockTime(@Param("person_id") Long person_id);
+
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE person SET blocked_account = false WHERE person_id = :person_id", nativeQuery = true)
+    void desblockUser(@Param("person_id") Long person_id);
 }
--- backend/src/main/java/com/carlosamaral/backend/repository/PlataformRepository.java
+++ backend/src/main/java/com/carlosamaral/backend/repository/PlataformRepository.java
@@ -2,7 +2,8 @@ package com.carlosamaral.backend.repository;
 
 import com.carlosamaral.backend.model.PlataformModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PlataformRepository extends JpaRepository<PlataformModel, Long> {
-
 }
