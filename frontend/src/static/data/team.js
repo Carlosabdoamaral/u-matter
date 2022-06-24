8f26ef3866daa0229a9434aca3eaf2390ab76bb4
Igor Rosiak <86728719+Igorrosiak@users.noreply.github.com>
2022-06-23 23:19:38 -0300

    Updating Quem Somos

---
+++ frontend/src/static/data/team.js
@@ -0,0 +1,21 @@
+import ImagemDev from "../files/img_desenvolvedor.png"
+
+export const teamData = [
+    {
+        name : "Igor Rosiak", 
+        area : "Dev. Fullstack", 
+        image : ImagemDev
+    },
+
+    {
+        name : "Fred Renan", 
+        area : "Dev. Fullstack", 
+        image : ImagemDev
+    },
+
+    {
+        name : "Carlos Amaral", 
+        area : "CEO | CTO", 
+        image : ImagemDev
+    },
+]
--- frontend/src/widgets/QuemSomosWidget.jsx
+++ frontend/src/widgets/QuemSomosWidget.jsx
@@ -1,3 +1,4 @@
+import { teamData } from "../static/data/team"
 import imagemDev from "../static/files/img_desenvolvedor.png"
 
 export const QuemSomosWidget = _ => {
@@ -5,36 +6,26 @@ export const QuemSomosWidget = _ => {
         <div className="quemSomos">
 
             <h1>Quem Somos?</h1>
-        
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dictum sem sapien, eu 
-                feugiat turpis congue rhoncus. Vivamus elit nulla, ullamcorper nec ullamcorper et, 
-                fermentum sed lectus. Nullam ornare nisi eros, tempus faucibus diam bibendum sit amet. 
-                Donec nunc sapien, porttitor et condimentum id, tincidunt in purus. Mauris dictum id 
-                purus cursus laoreet. Nunc posuere libero vitae consectetur vehicula. Aliquam fringilla 
-                mattis hendrerit. Donec mollis tortor eu tincidunt molestie. Proin finibus velit pulvinar 
-                dui auctor vestibulum. Vivamus quis mi id magna lacinia egestas. Donec imperdiet id augue 
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dictum sem sapien, eu
+                feugiat turpis congue rhoncus. Vivamus elit nulla, ullamcorper nec ullamcorper et,
+                fermentum sed lectus. Nullam ornare nisi eros, tempus faucibus diam bibendum sit amet.
+                Donec nunc sapien, porttitor et condimentum id, tincidunt in purus. Mauris dictum id
+                purus cursus laoreet. Nunc posuere libero vitae consectetur vehicula. Aliquam fringilla
+                mattis hendrerit. Donec mollis tortor eu tincidunt molestie. Proin finibus velit pulvinar
+                dui auctor vestibulum. Vivamus quis mi id magna lacinia egestas. Donec imperdiet id augue
                 non sodales. Vestibulum ac luctus
             </p>
 
-            <div className="container">            
+            <div className="container">
 
-                <div className="card">
-                    <img src= {imagemDev} alt="Foto CEO Carlos Amaral" />
-                    <span>CEO | CTO</span>
-                    <h2>Carlos Alberto Barcelos do Amaral</h2>
-                </div>
-
-                <div className="card">
-                    <img src= {imagemDev} alt="Foto Desenvolvedor Frederico" />
-                    <span>Dev. Fullstack</span>
-                    <h2>Frederico Renan Monteiro Motolla</h2>
-                </div>
-
-                <div className="card">
-                    <img src= {imagemDev} alt="Foto Desenvolvedor Igor" />
-                    <span>Dev. Fullstack</span>
-                    <h2>Igor Luiz Belter Rosiak</h2>
-                </div>
+                {teamData.map((obj, i) => (
+                    <div className="card">
+                        <img src={obj.image} alt= {`Imagem ${obj.area} ${obj.name}`} />
+                        <span>{obj.area}</span>
+                        <h2>{obj.name}</h2>
+                    </div>
+                ))}
 
             </div>
         </div>
